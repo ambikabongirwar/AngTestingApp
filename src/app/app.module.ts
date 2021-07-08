df78b24bf2442b826f12d4f5c54eba4c3513a1fd
ambikabongirwar@gmail.com <ambikabongirwar@gmail.com>
2021-07-09 02:40:22 +0530

    components architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductsDetailsComponent } from './products-details/products-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    FooterComponent,
+    ProductsDetailsComponent
   ],
   imports: [
     BrowserModule,
