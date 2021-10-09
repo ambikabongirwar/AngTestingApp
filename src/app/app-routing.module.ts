1fde95d1e7eb8b1ce899e56a19e3f104c70e3a83
ambikabongirwar@gmail.com <ambikabongirwar@gmail.com>
2021-10-10 00:08:56 +0530

    added register and login components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ProductsComponent } from './products/products.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'products', component: ProductsComponent},
   {path: 'register', component: RegisterComponent},
